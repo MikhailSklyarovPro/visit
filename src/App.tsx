f31e65a2b19b205ec8bb70b4dfbc833c232e04e7
Sklyarov Misha <mikhail.sklyarov.01@bk.ru>
2026-06-19 11:55:59 +0300

    Настроил навигацию

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-export const App = () => {
-  return <>Hello</>;
-};
-
-export default App;
---
+++ src/components/pages/login/login.tsx
@@ -0,0 +1,6 @@
+export const Login = () => {
+  return <div>
+    <button></button>
+    <input type="text" />
+  </div>;
+};
